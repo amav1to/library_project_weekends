329cbf3debedd2052b8358a2f3cbf99647eaa953
Рауан <midanovrauan08@gmail.com>
2025-12-26 15:57:56 +0500

    Добавлены зависимости в requiremets.txt(psycopg[binary,pool]==3.2.3sqlalchemy[postgresql]), исправлен config.py для перехода на postgre

--- config.py
+++ config.py
@@ -1,12 +1,6 @@
 import os
 
 class Config:
-    # Секретный ключ для защиты форм (сгенерируйте свой или оставьте так)
-    SECRET_KEY = 'ваш-секретный-ключ-сюда-12345'
-    
-    # Путь к базе данных SQLite (файл database.db будет создан в папке проекта)
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-    
-    # Отключаем систему уведомлений Flask-SQLAlchemy (не обязательно, но удобно)
+    SECRET_KEY = os.getenv('SECRET_KEY', 'ваш-секретный-ключ-сюда-12345')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///:memory:')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
